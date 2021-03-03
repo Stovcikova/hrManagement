3ab392f0e599bb7a9b88250be7a2b4517cd3ebdf
Stovcikova <henrieta.stovcikova@kosickaakademia.sk>
2021-03-03 22:52:14 +0100

    add database metoda na zobrazenie uzivatelov

--- src/main/java/sk/kosickaakademia/stovcikova/company/App.java
+++ src/main/java/sk/kosickaakademia/stovcikova/company/App.java
@@ -1,6 +1,9 @@
 package sk.kosickaakademia.stovcikova.company;
 
 import sk.kosickaakademia.stovcikova.company.database.Database;
+import sk.kosickaakademia.stovcikova.company.entity.User;
+
+import java.util.List;
 
 /**
  * Hello world!
@@ -13,6 +16,12 @@ public class App
 
         System.out.println( "Hello World!" );
         Database db=new Database();
-        db.getConnection();
+        db.insertNewUser(new User("Janko", "Hraško", 10,0));
+        db.getMales();
+        db.getFemales();
+        List<User> list=db.getMales();
+        System.out.println(list);
+
     }
+
 }
--- src/main/java/sk/kosickaakademia/stovcikova/company/database/Database.java
+++ src/main/java/sk/kosickaakademia/stovcikova/company/database/Database.java
@@ -1,31 +1,49 @@
 package sk.kosickaakademia.stovcikova.company.database;
 
+import sk.kosickaakademia.stovcikova.company.entity.User;
 import sk.kosickaakademia.stovcikova.company.log.Log;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
 
 
 public class Database {
-    String url = "jdbc:mysql://itsovy.sk:3306/company";
 
-    String username="mysqluser";
-    String password="Kosice2021!";
     Log log=new Log();
+    private final String InsertQuery ="INSERT INTO user (fname, lname, age, gender) "+
+            "VALUES (?, ?, ?, ?)";
 
 
 
-    public Connection getConnection(){
+    public Connection connect(){
         try {
-            Connection con= DriverManager.getConnection(url,username, password);
-            return con;
-        } catch (SQLException e) {
-            log.error(e.toString());
-        }
-        return  null;
+            Properties props=new Properties();
+            InputStream loader = getClass().getClassLoader().getResourceAsStream("db.properties");
+            props.load(loader);
+            String url= props.getProperty("url");
+            String username= props.getProperty("username");
+            String password= props.getProperty("password");
 
+            Connection con= DriverManager.getConnection(url,username, password);
+
+            if (con !=null){
+                System.out.println("Connected to company");
+                return con;
+            }else {
+                System.out.println("Connection failed");
+                return null;
+            }
+
+        } catch (IOException | SQLException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
+
     public void closeConnection(Connection con){
         if (con!=null){
             try {
@@ -38,4 +56,62 @@ public class Database {
         }
 
     }
+    //metoda na vlozenie noveho pouzivatela
+    public boolean insertNewUser(User user){
+        Connection connection=connect();
+        if(connection!=null){
+            try{
+                PreparedStatement ps=connection.prepareStatement(InsertQuery);
+                ps.setString(1, user.getFname());
+                ps.setString(2, user.getLname());
+                ps.setInt(3,user.getAge());
+                ps.setInt(4,user.getGender().getValue());
+                int result=ps.executeUpdate();
+                closeConnection(connection);
+                log.print("New user is added");
+                return result==1;
+            }catch (SQLException e){
+                log.error(e.toString());
+            }
+        }
+        return false;
+    }
+
+    public List<User> getMales(){
+        String sqlMale="SELECT * FROM user WHERE gender=0";
+        try{
+            PreparedStatement ps=connect().prepareStatement(sqlMale);
+            return executeSelect(ps);
+        }catch (Exception e){
+            log.error(e.toString());
+        }
+        return null;
+    }
+    public List<User> getFemales(){
+        String sqlFemale="SELECT * FROM user WHERE gender=1";
+        try{
+            PreparedStatement ps=connect().prepareStatement(sqlFemale);
+            return executeSelect(ps);
+        }catch (Exception e){
+            log.error(e.toString());
+        }
+        return null;
+    }
+
+//metoda na zobrazenie zoznamu uzivatelov
+    private List<User> executeSelect(PreparedStatement ps) throws SQLException {
+        ResultSet rs= ps.executeQuery();
+        List<User> list = new ArrayList<>();
+        while (rs.next()){
+            String fname = rs.getString("fname");
+            String lname = rs.getString("lname");
+            int age = rs.getInt("age");
+            int id = rs.getInt("id");
+            int gender = rs.getInt("gender");
+        }
+        return list;
+
+    }
+
+
 }
